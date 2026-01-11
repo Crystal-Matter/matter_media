4c5650e406a7f8bef7bed74bd627fd731ed9dece
Stephen von Takach <steve@place.technology>
2026-01-11 22:42:27 +1100

    chore: move clusters to core matter library

--- src/matter_media/matter.cr
+++ src/matter_media/matter.cr
@@ -2,6 +2,4 @@ require "matter"
 
 require "./matter/media_backend"
 require "./matter/commissioning_display"
-require "./matter/fixed_label_cluster"
-require "./matter/user_label_cluster"
 require "./matter/device"
